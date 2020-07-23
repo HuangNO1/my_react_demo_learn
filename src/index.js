1e6a716d03bdcba8a855360d9150e095c9cce15a
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-07-24 01:08:11 +0800

    JSX語法 style 註解 學習

--- src/index.js
+++ src/index.js
@@ -44,18 +44,62 @@ serviceWorker.unregister();
 
 // react 函數式組件
 // 組件開頭必須大寫
-function Clock(props) {
-  return (
-    <h1>Current Time: { props.date.toLocaleTimeString() }</h1>
-  );
-}
+// function Clock(props) {
+//   return (
+//     <h1>Current Time: { props.date.toLocaleTimeString() }</h1>
+//   );
+// }
 
-function run() {
-  // date 傳參
-  ReactDOM.render(
-    <Clock date={ new Date() } />,
-    document.querySelector('#root')
-  )
-}
+// function run() {
+//   // date 傳參
+//   ReactDOM.render(
+//     <Clock date={ new Date() } />,
+//     document.querySelector('#root')
+//   )
+// }
 
-setInterval(run, 1000);
+// setInterval(run, 1000);
+
+// style
+// 駝峰命名
+// style 中如果存在多個單字的屬性組合，第二個單字開始，首字母大寫。
+// 可以不首字母大寫，但要換寫法
+let exampleStyle = {
+  background: "skyblue",
+  borderBottom: "1px solid red",
+  // 不用首字母大寫的方法 e.g.
+  // "background-img": "url(https...)"
+}
+let element = (
+  <div>
+    <h1 style={exampleStyle}>Hello World</h1>
+  </div>
+)
+
+// class 在 Js 中是關鍵字，所以要改成 className
+// className 和 style 等屬性不能是 string，必須是對象參數
+// 不能有多個 className 或是多個 style，會被自動刪掉剩一個
+// className 可以用 string 相加的方式
+let classStr = "abc"
+
+let element1 = (
+  <div>
+    <h1 className={"cba " + classStr}>Hello World</h1>
+  </div>
+)
+
+// react 中 className array 無法像 Vue 一樣可以自動拆解
+// 所以要加上 join 去做間隔
+let classArray = ['abc', 'cba'].join(" ");
+let elementArrayClass = (
+  <div>
+    {/* 在 HTML 寫註釋 */}
+    <h1 className={classArray}>Hello World</h1>
+  </div>
+  // 在 JS 寫註釋
+)
+
+ReactDOM.render(
+  element1,
+  document.querySelector('#root')
+)
