161d787eaf581063b3c42d1be3a869c1e55ac05b
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-07-24 11:48:52 +0800

    學習 react 函數式組件 與 類組件

--- src/index.js
+++ src/index.js
@@ -64,42 +64,63 @@ serviceWorker.unregister();
 // 駝峰命名
 // style 中如果存在多個單字的屬性組合，第二個單字開始，首字母大寫。
 // 可以不首字母大寫，但要換寫法
-let exampleStyle = {
-  background: "skyblue",
-  borderBottom: "1px solid red",
-  // 不用首字母大寫的方法 e.g.
-  // "background-img": "url(https...)"
-}
-let element = (
-  <div>
-    <h1 style={exampleStyle}>Hello World</h1>
-  </div>
-)
+// let exampleStyle = {
+//   background: "skyblue",
+//   borderBottom: "1px solid red",
+//   // 不用首字母大寫的方法 e.g.
+//   // "background-img": "url(https...)"
+// }
+// let element = (
+//   <div>
+//     <h1 style={exampleStyle}>Hello World</h1>
+//   </div>
+// )
 
 // class 在 Js 中是關鍵字，所以要改成 className
 // className 和 style 等屬性不能是 string，必須是對象參數
 // 不能有多個 className 或是多個 style，會被自動刪掉剩一個
 // className 可以用 string 相加的方式
-let classStr = "abc"
+// let classStr = "abc"
 
-let element1 = (
-  <div>
-    <h1 className={"cba " + classStr}>Hello World</h1>
-  </div>
-)
+// let element1 = (
+//   <div>
+//     <h1 className={"cba " + classStr}>Hello World</h1>
+//   </div>
+// )
 
-// react 中 className array 無法像 Vue 一樣可以自動拆解
-// 所以要加上 join 去做間隔
-let classArray = ['abc', 'cba'].join(" ");
-let elementArrayClass = (
-  <div>
-    {/* 在 HTML 寫註釋 */}
-    <h1 className={classArray}>Hello World</h1>
-  </div>
-  // 在 JS 寫註釋
-)
+// // react 中 className array 無法像 Vue 一樣可以自動拆解
+// // 所以要加上 join 去做間隔
+// let classArray = ['abc', 'cba'].join(" ");
+// let elementArrayClass = (
+//   <div>
+//     {/* 在 HTML 寫註釋 */}
+//     <h1 className={classArray}>Hello World</h1>
+//   </div>
+//   // 在 JS 寫註釋
+// )
+
+// ReactDOM.render(
+//   element1,
+//   document.querySelector('#root')
+// )
+
+// 類組件 - 可以定義方法
+// 有事件或是動態的 使用類組件：e.g. 點擊事件
+// 函數式組件是靜態組件，頂多傳參數
+// 函數式組件 stateless 無生命週期
+// 類組件 stateful 有生命週期
+// 類組件中可以在包含組件 -> 複合組件
+class HelloWorld extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>類組件 Hello World</h1>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  element1,
+  <HelloWorld />,
   document.querySelector('#root')
 )
